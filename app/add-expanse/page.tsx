7daea281b4bd36ec2037b26268209d7c42e12cf1
tomas <72792707+tomas-land@users.noreply.github.com>
2023-01-08 22:32:46 +0200

    deploy test

--- app/add-expanse/page.tsx
+++ app/add-expanse/page.tsx
@@ -2,16 +2,14 @@
 
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
-import { useForm,Controller } from "react-hook-form";
-// import Select from "react-select";
+import { useForm } from "react-hook-form";
 
 import s from '@styles/_AddExpense.module.scss'
 
 
 const AddExpanse = () => {
   const router = useRouter()
-
-  const { register, handleSubmit,control, formState: { errors } } = useForm({
+  const { register, handleSubmit, control, formState: { errors } } = useForm({
     defaultValues: {
       amount: "",
       title: 'def',
@@ -20,17 +18,17 @@ const AddExpanse = () => {
   });
   const onSubmit = async ({ title, amount, expensesCategoryID }: any) => {
     console.log(title, amount, expensesCategoryID);
-    // try {
-    //   const body = { title, amount, expensesCategoryID };
-    //   await fetch(`/api/expenses`, {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify(body),
-    //   });
-    //   router.push("/");
-    // } catch (error) {
-    //   console.error(error);
-    // }
+    try {
+      const body = { title, amount, expensesCategoryID };
+      await fetch(`/api/expenses`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -38,38 +36,26 @@ const AddExpanse = () => {
       { /* "hand leSubmit" will validate your inputs before invoking "onSubmit" */}
       <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
         {/* register your input into the hook by invoking the "register" function */}
-        <input placeholder='0' {...register("amount", {
+        <div className={s.inputs}>
+          <input placeholder='0' {...register("amount", {
+            required: true,
+            valueAsNumber: true,
+          })} />
+          {/* <input placeholder='' {...register("amount", {
           required: true,
           valueAsNumber: true,
-        })} />
-
-        {/* include validation with required or other standard HTML validation rules */}
-        {/* <input placeholder='Title' {...register("title")} /> */}
-
-        {/* <input placeholder='category' {...register("expensesCategoryID", { required: true })} /> */}
-        {/* errors will return when field validation fails */}
-        {/* {errors.expensesCategoryID && <span>This field is required</span>} */}
-        {/* <select {...register("expensesCategoryID")} placeholder='category' >
-        <option value="Katef"  >Kategorija</option>
-        <option value="1">Maistas</option>
-        <option value="2">Kuras</option>
-      </select> */}
-        {/* <select {...register("expensesCategoryID")}>
-          {options.map(value => (
-            <option key={value} value={value}>
-              {value}
-            </option>
-          ))}
-        </select> */}
-         {/* <Controller
-            name="department"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <Select {...field} isMulti options={departments} />
-            )}
-          /> */}
-        <input type="submit" />
+        })} /> */}
+          <select {...register("expensesCategoryID", {
+            required: true,
+            valueAsNumber: true
+          })}>
+            <option value="" disabled style={{ display: 'none' }}>Kategorija</option>
+            <option value="1">Maistas</option>
+            <option value="2">Kuras</option>
+            <option value="2">Darbas</option>
+          </select>
+        </div>
+        <button type='submit'>IŠSAUGOTI</button>
       </form>
     </div>
   )
