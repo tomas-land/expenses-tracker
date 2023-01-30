952c119fa0bcbd37a17eff3fc9148fadd153af6e
tomas <72792707+tomas-land@users.noreply.github.com>
2023-01-30 23:30:07 +0200

    update

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -10,21 +10,20 @@ import { getCategoryWithExpenses } from '../../lib/prisma/expenses_by_category'
 import TotalDisplay from './TotalDisplay';
 
 
-async function getCategoryWithExpensesDB() {
-  const categoryWithExpenses = await getCategoryWithExpenses()
-  return categoryWithExpenses;
-}
-async function getTotalAmountExpensesDB() {
-  const totalAmountExpenses = await getTotalAmountExpenses()
-  return totalAmountExpenses;
-}
+// async function getCategoryWithExpensesDB() {
+//   const categoryWithExpenses = await getCategoryWithExpenses()
+//   return categoryWithExpenses;
+// }
+// async function getTotalAmountExpensesDB() {
+//   const totalAmountExpenses = await getTotalAmountExpenses()
+//   return totalAmountExpenses;
+// }
 
 const StatsPage = async () => {
-  const { totalAmountExpenses } = await getTotalAmountExpensesDB(); 
-  const  categoryWithExpenses  = await getCategoryWithExpensesDB();
+  // const { totalAmountExpenses } = await getTotalAmountExpensesDB(); 
+  // const  categoryWithExpenses  = await getCategoryWithExpensesDB();
   // const totalFoodExpenses = totalFoodExpenses.map((item:any) => item.amount).reduce((prev:number, curr:number) => prev + curr, 0);
-// console.log(categoryWithExpenses)
-console.log(categoryWithExpenses)
+
   return (
     <section className={s.stats_page}>
       <div className={s.top_btns}>
@@ -32,8 +31,8 @@ console.log(categoryWithExpenses)
         <Link href="/"><button className={s.back_btn} ><IoIosAdd /></button></Link>
       </div>
       
-      <TotalDisplay totalAmountExpenses={totalAmountExpenses} />
-      <TotalExpensesList categoryWithExpenses={categoryWithExpenses} />
+      {/* <TotalDisplay totalAmountExpenses={totalAmountExpenses} />
+      <TotalExpensesList categoryWithExpenses={categoryWithExpenses} /> */}
     </section>
   )
 }
