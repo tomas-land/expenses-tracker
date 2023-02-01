fdc5587d8d210cc63e0caf5d45c6db7765671e0a
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-01 10:40:35 +0200

    update

--- app/ExpensesListItem.tsx
+++ app/ExpensesListItem.tsx
@@ -31,10 +31,10 @@ const ExpensesListItem = ({ expense }: any) => {
       console.error(error);
     }
   }
-  const formatedDate = (date: string) => {
-    const formatedDate = Moment(date).format('YYYY-MM-DD H:mm');
-    return formatedDate;
-  }
+  // const formatedDate = (date: string) => {
+  //   const formatedDate = Moment(date).format('YYYY-MM-DD H:mm');
+  //   return formatedDate;
+  // }
 
   return (
     <li className={s.list_item}>
@@ -45,7 +45,7 @@ const ExpensesListItem = ({ expense }: any) => {
       <div className={s.amount}>-<span>{amount}</span><MdOutlineEuro color='gray' size={13} className={`${showExtraInfo ? s.rotate_up : null}`} /></div>
       <div className={`${s.extra_info} ${showExtraInfo && s.open}`}>
         {desc?.length > 0 && <div className={s.desc}>{desc}</div>}
-        <div className={s.date}>{formatedDate(createdAt)}</div>
+        <p className={s.date}>{createdAt}</p>
         <ImBin size={13} className={s.delete_btn} onClick={() => deleteExpense(id)} />
       </div>
     </li>
