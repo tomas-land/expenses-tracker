f26559f9ced31f4ce9147ecd3aba81724719220a
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-01 11:00:15 +0200

    update

--- app/ExpensesListItem.tsx
+++ app/ExpensesListItem.tsx
@@ -37,17 +37,7 @@ console.log(expense);
 
   return (
     <li className={s.list_item}>
-      <div>
-        f
-        <button className={s.toggle_btn} onClick={toggleExtraInfo}><GrFormDown /></button>
-        {/* {categories[0].category.name} */}
-      </div>
-      <div className={s.amount}>-<span>{amount}</span><MdOutlineEuro color='gray' size={13} className={`${showExtraInfo ? s.rotate_up : null}`} /></div>
-      <div className={`${s.extra_info} ${showExtraInfo && s.open}`}>
-        {desc?.length > 0 && <div className={s.desc}>{desc}</div>}
-        <p className={s.date}>{createdAt}</p>
-        <ImBin size={13} className={s.delete_btn} onClick={() => deleteExpense(id)} />
-      </div>
+      ff
     </li>
   )
 }
