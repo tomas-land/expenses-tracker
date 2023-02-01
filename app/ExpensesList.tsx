01e7d9c12205de3373a9a0b95bbcc0d05c45832b
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-01 10:00:43 +0200

    update

--- app/ExpensesList.tsx
+++ app/ExpensesList.tsx
@@ -19,7 +19,8 @@ const ExpensesList = ({ expenses }: any) => {
       <ul className={s.list}>
         {expenses?.map((expense: any) => {
           return (
-            <ExpensesListItem key={expense.id} expense={expense} />
+            <></>
+            // <ExpensesListItem key={expense.id} expense={expense} />
           )
         })}
       </ul>
