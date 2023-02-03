63bad6b0ab015a70267835b89d81f0c7bb941cd2
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-03 09:47:15 +0200

    updatedd

--- app/ExpensesList.tsx
+++ app/ExpensesList.tsx
@@ -13,7 +13,7 @@ const ExpensesList = (expenses : any) => {
     router.refresh()
   }, [router])
   // console.dir(expenses, { depth: null });
-  console.log(expenses);
+  console.log(expenses.expenses[0].expenses[0].expense.amount);
   return (
     <div className={s.expenses_list}>
       <h3>Paskutiniai atsiskaitymai</h3>
