0389e0b84d1c086b788bb42e8b51429b4ea3b2ff
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-03 11:01:06 +0200

    first commit

--- app/ExpensesList.tsx
+++ app/ExpensesList.tsx
@@ -13,12 +13,12 @@ const ExpensesList = ({expenses} : any) => {
     router.refresh()
   }, [router])
   // console.dir(expenses, { depth: null });
-  // console.log(expenses.expenses[0].expenses[0].expense.amount);
+  console.log(expenses);
   return (
     <div className={s.expenses_list}>
       <h3>Paskutiniai atsiskaitymai</h3>
       <div>
-      {expenses.map((item:any) => {
+      {expenses?.map((item:any) => {
         return (
           <div key={item.id}>
             <h1>{item.name}</h1>
