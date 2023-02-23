89727ae860165e509b9654064bd9e16747d0464c
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-24 01:06:52 +0200

    feature:add chart of expenses by category

--- app/statistics/Chart.tsx
+++ app/statistics/Chart.tsx
@@ -1,10 +1,30 @@
 "use client"
 
-import { PieChart, Pie, Cell, Tooltip } from "recharts";
-import s from "@styles/Components/_Chart.module.scss";
-import { startOFMonth } from '@lib/dayJS'
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+const data = [
+  {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+  {name: 'Page A', uv: 300, pv: 2400, amt: 2400}
+
+];
+
+const Chart = () => {
+
+  return (
+<LineChart width={300} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" />
+    <XAxis dataKey="name" />
+    <YAxis />
+  </LineChart>
+  );
+}
+export default Chart;
+// import React, { useCallback, useState, useEffect } from "react";
+// import { PieChart, Pie, Cell, Tooltip } from "recharts";
+// import s from "@styles/Components/_Chart.module.scss";
+// import { startOFMonth } from '@lib/dayJS'
+
 
 // const COLORS = [
 //   { start: "#9e54ed", end: "#5c4cb6" },
@@ -14,53 +34,99 @@ const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 //   { start: "#424242", end: "#424242" }
 // ];
 
-const data01 = [
-  { name: "Active Campagins", value: 90 },
-  { name: "Inactive Campagins", value: 25 },
-  { name: "ICPs with no campagins", value: 10 }
-];
+// const Chart = ({ categoriesWithExpenses }: any) => {
 
-const Chart = ({ categoriesWithExpenses }: any) => {
-  // const currentMonthTotalExpensesAmountByCategoryArr = categoriesWithExpenses.map((category: any) => {
-  //   const currentMonthExpensesArr = category.expenses.filter((expense: any) => {
-  //     return expense.createdAt > startOFMonth
-  //   })
-  //   const totalAmountByCategory = currentMonthExpensesArr?.map((expense: any) => expense.amount).reduce((prev: number, curr: number) => prev + curr, 0);
-  //   return { name: category.name, amount: totalAmountByCategory }
-  // })
+//   const currentMonthTotalExpensesAmountByCategoryArr = categoriesWithExpenses.map((category: any) => {
+//     const currentMonthExpensesArr = category.expenses.filter((expense: any) => {
+//       return expense.createdAt > startOFMonth
+//     })
+//     const totalAmountByCategory = currentMonthExpensesArr?.map((expense: any) => expense.amount).reduce((prev: number, curr: number) => prev + curr, 0);
+//     return { name: category.name, amount: totalAmountByCategory }
+//   })
 
-  // const categoriesWithExpensesByAmountArr = currentMonthTotalExpensesAmountByCategoryArr.sort((a: any, b: any) => b.amount - a.amount)
-  // const categoriesWithHighestTotalAmountArr = categoriesWithExpensesByAmountArr.slice(0, 4)
-  // const categoriesWithLowestTotalAmountArr = categoriesWithExpensesByAmountArr.slice(4, categoriesWithExpensesByAmountArr.length)
-  // const sumOfLowestTotalAmounts = categoriesWithLowestTotalAmountArr.map((item: any) => item.amount).reduce((prev: number, curr: number) => prev + curr, 0)
+//   const categoriesWithExpensesByAmountArr = currentMonthTotalExpensesAmountByCategoryArr.sort((a: any, b: any) => b.amount - a.amount)
+//   const categoriesWithHighestTotalAmountArr = categoriesWithExpensesByAmountArr.slice(0, 4)
+//   const categoriesWithLowestTotalAmountArr = categoriesWithExpensesByAmountArr.slice(4, categoriesWithExpensesByAmountArr.length)
+//   const sumOfLowestTotalAmounts = categoriesWithLowestTotalAmountArr.map((item: any) => item.amount).reduce((prev: number, curr: number) => prev + curr, 0)
 
-  // const isAmountHigherThanZero = categoriesWithHighestTotalAmountArr.some((item: any) => item.amount > 0)
-  // if (isAmountHigherThanZero) {
-  //   categoriesWithHighestTotalAmountArr.push({ name: 'Kita', amount: sumOfLowestTotalAmounts })
-  // }
+//   const isAmountHigherThanZero = categoriesWithHighestTotalAmountArr.some((item: any) => item.amount > 0)
+//   if (isAmountHigherThanZero) {
+//     categoriesWithHighestTotalAmountArr.push({ name: 'Kita', amount: sumOfLowestTotalAmounts })
+//   }
 
-  return (
-    <div className={s.chart}>
-      <PieChart width={300} height={300}>
-        <Pie
-          data={data01}
-          dataKey="value"
-          cx={200}
-          cy={200}
-          innerRadius={80}
-          outerRadius={100}
-        >
-          {data01?.map((entry, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={COLORS[index % COLORS.length]}
-            />
-          ))}
+//   return (
+//     <div className={s.chart}>
+//       <PieChart width={350} height={360}>
+//         <defs>
+//           {categoriesWithHighestTotalAmountArr?.map((entry: any, index: any) => (
+//             <linearGradient id={`myGradient${index}`} key={index}>
+//               <stop
+//                 offset="0%"
+//                 stopColor={COLORS[index % COLORS.length].start}
+//               />
+//               <stop
+//                 offset="100%"
+//                 stopColor={COLORS[index % COLORS.length].end}
+//                 stopOpacity={0.8}
+//               />
+//             </linearGradient>
+//           ))}
+//         </defs>
+//         <Pie
+//           data={categoriesWithHighestTotalAmountArr}
+//           // cx={200}
+//           // cy={150}
+//           label={({
+//             cx,
+//             cy,
+//             midAngle,
+//             innerRadius,
+//             outerRadius,
+//             percent,
+//             index,
+//           }: any) => {
+//             const RADIAN = Math.PI / 180;
+//             // eslint-disable-next-line
+//             const radius = 25 + innerRadius + (outerRadius - innerRadius);
+//             // eslint-disable-next-line
+//             const x = cx + radius * Math.cos(-midAngle * RADIAN);
+//             // eslint-disable-next-line
+//             const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-        </Pie>
-      </PieChart>
-    </div>
-  );
-}
+//             return (
+//               <>
+//                 <text
+//                   style={{ fontSize: "10px" }}
+//                   x={x}
+//                   y={y}
+//                   fill="gray"
+//                   textAnchor={x > cx ? "start" : "end"}
+//                   dominantBaseline="central"
 
-export default Chart;
+//                 >
+//                   {`${categoriesWithHighestTotalAmountArr[index].name}`}
+//                 </text>
+//                 <text style={{ fontSize: "8px" }} x={x} y={y + 10} fill="gray" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+//                   {`${(percent * 100).toFixed(0)}%`} </text>
+//               </>
+
+//             );
+//           }}
+//           isAnimationActive={true}
+//           outerRadius={90}
+//           innerRadius={55}
+//           cornerRadius={7}
+//           dataKey="amount"
+//           // paddingAngle={5}
+//           // innerRadius={isMobile ? 60 : 80}
+//         >
+//           {categoriesWithHighestTotalAmountArr?.map((entry: any, index: any) => (
+//             <Cell key={`cell-${index}`} fill={`url(#myGradient${index})`} />
+//           ))}
+//         </Pie>
+//       </PieChart>
+//     </div>
+//   );
+// }
+
+// export default Chart;
