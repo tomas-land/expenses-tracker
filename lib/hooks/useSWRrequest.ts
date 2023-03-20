9214dff19431e0883c03133b7aa33372f1d6e1fb
tomas <72792707+tomas-land@users.noreply.github.com>
2023-03-20 22:12:40 +0200

    update

--- lib/hooks/useSWRrequest.ts
+++ lib/hooks/useSWRrequest.ts
@@ -3,22 +3,12 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 // const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const useCategoriesSWR = () => {
-  const { data, error } = useSWR(`/api/categories`, fetcher);
-
-  return {
-    categories: data,
-    error,
-  };
-};
-
 export const useExpensesSWR = () => {
   const { data, mutate, error, isLoading } = useSWR("/api/expenses", fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     // revalidateOnMount: false
-  
   });
 
   return {
