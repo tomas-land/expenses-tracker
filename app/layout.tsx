d2faa80d69328693dfd92f5bb9e6e8c9c02405d4
tomas <72792707+tomas-land@users.noreply.github.com>
2023-04-11 22:22:10 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ export default function RootLayout({ children }: { children: any }) {
       <body>
         <main>
           <div className="wrapper">
+            <Analytics />
             <AppProvider>
               {children}
-              <Analytics />
             </AppProvider>
           </div>
         </main>
