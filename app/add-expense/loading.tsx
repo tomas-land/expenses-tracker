73c212b2b98ced12128420b5978cf466d8f466b8
tomas <72792707+tomas-land@users.noreply.github.com>
2023-05-18 23:27:24 +0300

    feat:added page loader

---
+++ app/add-expense/loading.tsx
@@ -0,0 +1,11 @@
+import "@styles/globals.scss"
+
+const loading = () => {
+  return (
+    <div className="loader">
+      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default loading
